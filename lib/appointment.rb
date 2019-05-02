04970e6ecbcb6b671d39ff7346e88f45f8e5491e
Gregory Losco <gtlosco@gmail.com>
2019-05-02 23:56:20 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -2,10 +2,12 @@ class Appointment
   
   attr_accessor :date, :patient, :doctor
   
+  @@all = []
+  
   def initialize(date, patient, doctor)
     @patient = patient 
     @doctor = doctor 
-    
+    @@all << self 
   end
   
    def self.all 
